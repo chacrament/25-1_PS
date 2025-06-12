878be1e69c62c86c3555e1b58a6b4a4d920feb21
chacrament <sungchan45654@gmail.com>
2025-06-12 22:07:21 +0900

    modified Main

--- Java/Main.java
+++ Java/Main.java
@@ -2,7 +2,7 @@ package Java;
 
 public class Main {
     public void run() {
-        System.out.println("Hello Worldadfadf!! My name is Seongchan Kim, nice to meet yasfasddafadsfaasdfafou! but I dfafddon't have dsafdenouadfgh money! I'm sorry about thadfafasdft. Really reallys. yfdfdedfadfafdap ye-ap, uhuh, thank you very so much... dkfkdksdsdsffdfasfdf");
+        System.out.println("Hello Worldadfadf!! My name is Seongchan Kim, nice to meet yasfasddafadsfaasdfafou! but I dfafddon't have dsafdenouadfgh money! I'm sorry about thadfafasdft. Really reallys. yfdfdedfadfafdap ye-ap, uhuh, thank ydfadfaou very so much... dkfkdksdsdsffdfasfdf");
     }
 
     public static void main(String[] args) {
